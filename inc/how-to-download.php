a004766f9e327139045bdefbbf67eb5ec9b9da5f
olcgit@gmail.com <=>
2020-01-24 06:53:06 +0100

    Instructions added

--- inc/how-to-download.php
+++ inc/how-to-download.php
@@ -1,5 +1,5 @@
 <div class="container my-5">
-	<h5>Free online Facebook video downloader</h5>
+	<h3>Free online Facebook video downloader</h3>
 	<p>
 		Eissaweb is one of the best tools available online for convert videos from Facebook to mp4 (video) or mp3 (audio) files and download them for free - this service works for computers, tablets and mobile devices.
 
@@ -11,7 +11,7 @@
 	</p>
 	<div class="row">
 		<div class="col-12 col-md-6">
-			<h5 class="">How to use Facebook video downloader ? </h5>
+			<h3 class="">How to use Facebook video downloader ? </h3>
 			<ol>
 				<li>First get the Facebook video url and copy it </li>
 				<li>paste the list and hit start</li>
@@ -19,7 +19,7 @@
 			</ol>
 		</div>
 		<div class="col-12 col-md-6">
-			<h5>Features: </h5>
+			<h3>Features: </h3>
 			<ul>
 				<li>It free to use :)</li>
 				<li>No registration required</li>
@@ -28,11 +28,11 @@
 			</ul>
 		</div>
 	</div>
-	<h5>Where are Facebook videos saved after being downloaded?</h5>
+	<h3>Where are Facebook videos saved after being downloaded?</h3>
 	<p>When you're downloading files, they are usually saved into whatever folder you have set as your default. Your browser normally sets this folder for you. In browser settings you can change and choose manually the destination folder for your downloded facebook videos.</p>
-	<h5>Can I download Live Facebook videos?</h5>
+	<h3>Can I download Live Facebook videos?</h3>
 	<p>Once streaming is done, you can easily save live FB videos to your device using Eissaweb Facebook video Downloader.</p>
-	<h5>Does Eissaweb store downloaded videos or keep a copy of videos?</h5>
+	<h3>Does Eissaweb store downloaded videos or keep a copy of videos?</h3>
 	<p>
 		Eissaweb doesn't store videos. Neither do we keep copies of downloaded videos. All videos are hosted on Facebook's servers. Also, we don't keep track of the download histories of our users, thus making using Eissaweb totally anonymous.
 	</p>
