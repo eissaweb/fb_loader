7705bb9999e764f3025fcfbb8c8e9e7694c1d62b
eissaweb <=>
2020-01-25 10:54:46 +0100

    File size added

--- app/main.php
+++ app/main.php
@@ -1,5 +1,5 @@
 <?php
-
+//error_reporting(0);
 header('Content-Type: application/json');
 
 $msg = [];
@@ -29,10 +29,12 @@ try {
     // get the thumbnail
     $msg['thumbnail'] = "https://graph.facebook.com/{$msg['id']}/picture";
     if ($sdLink = getSDLink($data)) {
+        $msg['sd_size'] = getFileSize($sdLink);
         $msg['links']['download_low'] = urlencode($sdLink);
     }
 
     if ($hdLink = getHDLink($data)) {
+        $msg['hd_size'] = getFileSize($hdLink);
         $msg['links']['download_high'] = urlencode($hdLink);
     }
     $msg['links']['play_video'] = $hdLink ?  $hdLink :  $sdLink;
@@ -124,4 +126,32 @@ function getDescription($curl_content)
     }
 
     return false;
+}
+
+
+function getFileSize($url, $mb = true) {
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_HEADER, true); 
+    curl_setopt($ch, CURLOPT_NOBODY, true); // make it a HEAD request
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+    $head = curl_exec($ch);
+
+    $mimeType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+    $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+    $path = parse_url($url, PHP_URL_PATH);
+    //$filename = substr($path, strrpos($path, '/') + 1);
+
+    curl_close($ch); 
+    if (! $mb) {
+        return $size;
+    }
+
+    $size = $size  / (1024 * 1024);
+    if ($size >= 1024) {
+        return round($size, 2) . ' GB';
+    }
+    $size = round($size, 2);
+    return $size . ' MB';
 }
--- extra.php
+++ extra.php
@@ -1,19 +1,32 @@
 <?php 
-$url = 'http://localhost/fb_video_core/download.php?url=https%3A%2F%2Fvideo-gru1-1.xx.fbcdn.net%2Fv%2Ft42.9040-2%2F82604248_1269287039936307_640447892338769920_n.mp4%3F_nc_cat%3D104%26efg%3DeyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9%26_nc_ohc%3D02HN3nxxO-cAX8QPTBt%26_nc_ht%3Dvideo-gru1-1.xx%26oh%3Dc9c1b9e762726c126743d9b39a03f2c2%26oe%3D5E29D5A6&filename=%E2%80%AB%D8%AC%D9%88_%D8%B4%D9%88_Joe_Show_-_%D9%85%D9%82%D8%AA%D9%84_%D9%82%D8%A7%D8%B3%D9%85_%D8%B3%D9%84%D9%8A%D9%85%D8%A7%D9%86%D9%8A_|_Facebook%E2%80%AC';
+$url = 'https://r1---sn-4g5edns6.googlevideo.com/videoplayback?expire=1579966244&ei=wwosXp7IGKrR5gTFrJSICw&ip=154.73.161.130&id=o-AMUTZDEMBnMHhSEd0LnH1QTIqHHJ-iDn65mfQbJXrNl6&itag=136&aitags=133%2C134%2C135%2C136%2C160%2C242%2C243%2C244%2C247%2C278&source=youtube&requiressl=yes&mm=31%2C26&mn=sn-4g5edns6%2Csn-aigl6nek&ms=au%2Conr&mv=m&mvi=0&pl=24&initcwndbps=103750&vprv=1&mime=video%2Fmp4&gir=yes&clen=1277592672&dur=7629.080&lmt=1576789236837519&mt=1579944594&fvip=1&keepalive=yes&fexp=23842630&c=WEB&txp=6316222&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=ALgxI2wwRQIgbTB_a1bZHuKlXZtw30WYDnFjKAsHgrOBCnnL32ZAREECIQD0zu0UAcQowHjmGI9ZDfkdNiMUgsAW0ntRm3ZtXnVDhw%3D%3D&lsparams=mm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AHylml4wRAIgKto-dw76hG-CvsWPIEwok_olw5VZc9yKlYDvp5YABGACIACqPterCs858LtpTRQAyJuP9qHFlw3SMdo_V6_fwuW4';
+
+//$url = urldecode($url);
 
 echo retrieve_remote_file_size($url);
 
 
-function retrieve_remote_file_size($url){
-     $ch = curl_init($url);
+function retrieve_remote_file_size($url, $mb = true){
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_HEADER, true); 
+    curl_setopt($ch, CURLOPT_NOBODY, true); // make it a HEAD request
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+    $head = curl_exec($ch);
 
-     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-     curl_setopt($ch, CURLOPT_HEADER, TRUE);
-     curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+    $mimeType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+    $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+    $path = parse_url($url, PHP_URL_PATH);
+    //$filename = substr($path, strrpos($path, '/') + 1);
 
-     $data = curl_exec($ch);
-     $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+    curl_close($ch); 
+    if (! $mb) {
+    	return $size;
+    }
 
-     curl_close($ch);
-     return $size;
+    $size = $size  / (1024 * 1024);
+    $size = round($size, 2);
+    return $size . ' MB';
+
 }
